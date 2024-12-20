228fc367d0200d8de929f0f8b741308b27ad7b0b
anton <kamaeff2@gmail.com>
2024-12-21 00:00:53 +0300

    fix frontend

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -34,11 +34,7 @@ const AuthenticatedLayout: React.FC<{
         <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
           <div className='flex h-16 justify-between'>
             <div className='flex'>
-              <div className='flex shrink-0 items-center'>
-                <Link href='/'>
-                  <ApplicationLogo className='block h-9 w-auto fill-current text-gray-800' />
-                </Link>
-              </div>
+                <ApplicationLogo className='block w-9 fill-current text-gray-800' />
 
               <div className='hidden space-x-8 sm:-my-px sm:ms-10 sm:flex'>
                 {links.map(link => (
